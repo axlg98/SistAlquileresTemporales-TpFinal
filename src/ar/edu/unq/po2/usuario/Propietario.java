16988af9a0e4700d681380b8e2d4ed01bfcf38dd
Damian Manchali <dami.manchali.eze@gmail.com>
2024-10-14 01:13:13 -0300

    agregando lista de inmuebles en propietario importando package

--- src/ar/edu/unq/po2/usuario/Propietario.java
+++ src/ar/edu/unq/po2/usuario/Propietario.java
@@ -1,9 +1,14 @@
 package ar.edu.unq.po2.usuario;
+import ar.edu.unq.po2.sistemaAlquiler.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Propietario extends Usuario{
-
+	private List<Inmueble> inmuebles = new ArrayList<Inmueble>();
 	public Propietario(String nombreCompleto, String telefono, String mail) {
 		super(nombreCompleto,telefono,mail);
 	}
+	
 
 }
