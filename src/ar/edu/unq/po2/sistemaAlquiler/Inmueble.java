dbf6996aee8fb5c6d29b5e03aa152ef8ec366ef3
axlg98 <axlgarcia080498@gmail.com>
2024-10-19 18:27:08 -0300

    Avanzando con atributos y métodos

--- src/ar/edu/unq/po2/sistemaAlquiler/Inmueble.java
+++ src/ar/edu/unq/po2/sistemaAlquiler/Inmueble.java
@@ -1,9 +1,47 @@
 package ar.edu.unq.po2.sistemaAlquiler;
 
-public class Inmueble {
+import java.time.LocalTime;
+import java.util.List;
 
-	public Inmueble() {
-		// TODO Auto-generated constructor stub
+public class Inmueble {
+	
+	private String tipoInmueble;
+	private String pais;
+	private Double superficie;
+	private String ciudad;
+	private String direccion;
+	private LocalTime checkIn;
+	private LocalTime checkOut;
+	private List<String> formasDePago;
+	private List<String> fotos;
+	
+	public Inmueble(String tipoInmueble, String pais, Double superficie, String ciudad, String direccion,
+			LocalTime checkIn, LocalTime checkOut) {
+		this.tipoInmueble = tipoInmueble;
+		this.pais = pais;
+		this.superficie = superficie;
+		this.ciudad = ciudad;
+		this.direccion = direccion;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
 
+	public void addFormaDePago(String formaDePago) {
+		this.formasDePago.add(formaDePago);
+	}
+	
+	public void addFoto(String foto) {
+		if(this.getFotos().size() == 5) {
+			System.out.println("Se alcanzó el máximo permitido de fotos.");
+		}else {
+			this.getFotos().add(foto);
+		}
+	}
+
+	public List<String> getFotos() {
+		return fotos;
+	}
+	
+	
+	
 }
