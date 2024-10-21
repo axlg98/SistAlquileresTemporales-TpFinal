ba1ed6d269f579347ae57cf5e427918e50950893
axlg98 <axlgarcia080498@gmail.com>
2024-10-21 17:21:19 -0300

    Avanzando

--- src/ar/edu/unq/po2/sistemaAlquiler/SistemaAlquiler.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.po2.sistemaAlquiler;
-
-public class SistemaAlquiler {
-
-	public SistemaAlquiler() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- src/ar/edu/unq/po2/usuario/Inquilino.java
+++ src/ar/edu/unq/po2/usuario/Inquilino.java
@@ -1,9 +1,29 @@
 package ar.edu.unq.po2.usuario;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unq.po2.sistemaAlquiler.Inmueble;
+
 public class Inquilino extends Usuario{
 
 	public Inquilino(String nombreCompleto, String telefono, String mail) {
 		super(nombreCompleto,telefono,mail);
 	}
+	
+	//ARREGLAR PARA SELECCIONAR EL INMUEBLE DEL SAT
+	
+	public Inmueble seleccionarInmueble(Inmueble inmueble) {
+		Inmueble inms = inmueble;
+		return inms;
+	}
+	
+	//ARREGLAR PARA BUSCAR ALQUILE DE LOS INMUEBLES DEL SAT
 
+	public List<Inmueble> buscarAlquiler(String ciudad, LocalDate fechaEntrada, LocalDate fechaSalida, int cantHuespuedes, Double minPrecio, Double maxPrecio){
+		List<Inmueble> inmuebles = new ArrayList<Inmueble>();
+		return inmuebles;
+	}
+	
 }
