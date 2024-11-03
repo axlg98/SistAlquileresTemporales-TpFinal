c92103cc88e65042841ce9bc3eefc25330d35730
Damian Manchali <dami.manchali.eze@gmail.com>
2024-11-02 22:21:30 -0300

    agregando Politicas y sus respectivas clases

---
+++ src/ar/edu/unq/po2/sistemaAlquiler/SinCancelacion.java
@@ -0,0 +1,29 @@
+package ar.edu.unq.po2.sistemaAlquiler;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import ar.edu.unq.po2.reserva.Reserva;
+
+
+
+
+public class SinCancelacion implements IPoliticaDeCancelacion {
+
+	@Override
+	public Double costoDeCancelacion(Reserva reserva, LocalDate fechaCancelacion, Double precio) {
+		
+		// cuantos dias hay dentro del rango de fechas
+		
+		long totalDias = ChronoUnit.DAYS.between(reserva.getFechaInicio(), reserva.getFechaFin());
+		
+		return totalDias * precio;
+	}
+
+	
+
+
+
+	
+	
+}
