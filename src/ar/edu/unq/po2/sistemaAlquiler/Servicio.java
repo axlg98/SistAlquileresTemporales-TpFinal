79d370dcc34ceab8e1f6faf9cf2fda7448ae415c
axlg98 <axlgarcia080498@gmail.com>
2024-11-03 23:48:08 -0300

    .

---
+++ src/ar/edu/unq/po2/sistemaAlquiler/Servicio.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.sistemaAlquiler;
+
+public class Servicio {
+	
+	private String nombre;
+	
+	public Servicio(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getNombre() {
+		return this.nombre;
+	}
+
+}
