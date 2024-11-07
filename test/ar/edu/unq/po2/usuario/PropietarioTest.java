0928c7ee59c040b95a6ddba99c03576d8554f8d2
axlg98 <axlgarcia080498@gmail.com>
2024-11-07 02:25:24 -0300

    Avanzando test Propietario

--- test/ar/edu/unq/po2/usuario/PropietarioTest.java
+++ test/ar/edu/unq/po2/usuario/PropietarioTest.java
@@ -1,7 +1,9 @@
 package ar.edu.unq.po2.usuario;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -17,12 +19,14 @@ import ar.edu.unq.po2.sistemaAlquiler.Inmueble;
 
 class PropietarioTest {
 	
-	Inmueble i1;
-	Inmueble i2;
+	Inmueble inmueble1;
+	Inmueble inmueble2;
 	
-	Propietario p;
+	Propietario propietario;
 	
 	Reserva reserva;
+	Reserva r2;
+	
 	EstadoReserva aceptada;
 	EstadoReserva pendiente;
 	EstadoReserva cancelada;
@@ -31,53 +35,96 @@ class PropietarioTest {
 	@BeforeEach
 	void setUp(){
 		
-		p = new Propietario("p1","11111111", "example@gmail.com");
+		propietario = new Propietario("p1","11111111", "example@gmail.com");
 		
-		i1 = mock(Inmueble.class);
-		i2 = mock(Inmueble.class);
+		inmueble1 = mock(Inmueble.class);
+		inmueble2 = mock(Inmueble.class);
 		
 		reserva = mock(Reserva.class);
+		r2 = mock(Reserva.class);
 		
 		aceptada = mock(ReservaAceptada.class);
 		pendiente   = mock(ReservaPendiente.class);
 		cancelada  = mock(ReservaCancelada.class);
 		completada = mock(ReservaCompletada.class);
 		
-		p.addInmueble(i1);
-		p.addInmueble(i2);
+		propietario.addInmueble(inmueble1);
+		propietario.addInmueble(inmueble2);
 	}
 
 	@Test
 	void PropietarioAceptandoReservaDelInquilinoTest() {
-		when(reserva.getEstado()).thenReturn(pendiente);
-		p.aceptarReservaInquilino(reserva);
+		when(reserva.getEstado()).thenReturn(aceptada);
+		propietario.aceptarReservaInquilino(reserva);
 		assertTrue(reserva.getEstado() instanceof ReservaAceptada);
 	}
 	
 	@Test
 	void PropietarioCancelandoReservaDelInquilinoTest() {
-		p.cancelarReservaInquilino(reserva);
+		when(reserva.getEstado()).thenReturn(cancelada);
+		propietario.cancelarReservaInquilino(reserva);
 		assertTrue(reserva.getEstado() instanceof ReservaCancelada);
 	}
 	
 	@Test
 	void PropietarioAceptandoReservaDelInquilinoEstandoACeptadaTest() {
+		propietario.aceptarReservaInquilino(reserva);
 		when(reserva.getEstado()).thenReturn(aceptada);
-		p.aceptarReservaInquilino(reserva);
+		propietario.aceptarReservaInquilino(reserva);
 		
 		//No hay nada más para verificar.
+		//verify(reserva).setEstado(any(ReservaAceptada.class));
 		
-		assertFalse(reserva.getEstado() instanceof ReservaAceptada);
+		assertTrue(reserva.getEstado() instanceof ReservaAceptada);
 	}
 	
 	@Test
-	void PropietarioAceptandoReservaDelInquilinoEstandoCanceladaTest() {
+	void PropietarioCancelandoReservaDelInquilinoEstandoCanceladaTest() {
 		when(reserva.getEstado()).thenReturn(cancelada);
-		p.cancelarReservaInquilino(reserva);
+		propietario.cancelarReservaInquilino(reserva);
+		propietario.cancelarReservaInquilino(reserva);
 		
 		//No hay nada más para verificar.
 		
-		assertFalse(reserva.getEstado() instanceof ReservaCancelada);
+		assertTrue(reserva.getEstado() instanceof ReservaCancelada);
+	}
+	
+	@Test
+	void cantidadDeInmueblesTest() {
+		assertEquals(propietario.getInmuebles().size(),2);
+	}
+	
+	@Test
+	void eliminarUnInmuebleTest() {
+		propietario.eliminarInmueble(inmueble1);
+		assertEquals(propietario.getInmuebles().size(),1);
+	}
+	
+	@Test
+	void cantidadDeReservasAceptadasDelInmuebleTest() {
+		
+		//Arreglar
+		
+		when(reserva.getEstado()).thenReturn(aceptada);
+		when(r2.getEstado()).thenReturn(cancelada);
+		propietario.aceptarReservaInquilino(reserva);
+		propietario.aceptarReservaInquilino(r2);
+		inmueble1.addReserva(reserva);
+		inmueble2.addReserva(r2);
+		
+		
+		assertEquals(propietario.todasLasReservasAceptadas().size(),0);
+	}
+	
+	//Probando un Spy
+	
+	@Test
+	void pruebaSpyTest() {
+		
+		inmueble1.addReserva(reserva);
+		
+		verify(inmueble1).addReserva(any(Reserva.class));
+		
 	}
 
 }
