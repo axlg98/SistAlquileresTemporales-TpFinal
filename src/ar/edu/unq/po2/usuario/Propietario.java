280cf714ee795c32289c1dd5f76a15a782b96c7c
axlg98 <axlgarcia080498@gmail.com>
2024-11-09 01:59:18 -0300

    Modificando clase Usuario y Propietario, y test a arreglar.

--- src/ar/edu/unq/po2/usuario/Propietario.java
+++ src/ar/edu/unq/po2/usuario/Propietario.java
@@ -1,71 +1,14 @@
 package ar.edu.unq.po2.usuario;
-import ar.edu.unq.po2.reserva.Reserva;
-import ar.edu.unq.po2.sistemaAlquiler.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
-public class Propietario extends Usuario{
-	private List<Inmueble> inmuebles;
-	
-	public Propietario(String nombreCompleto, String telefono, String mail) {
-		super(nombreCompleto,telefono,mail);
-		inmuebles = new ArrayList<Inmueble>();
-	}
-	
-	//Aceptar la Reserva de un Inquilino
-	
-	public void aceptarReservaInquilino(Reserva reserva) {
-		if(reserva.isConfirmada()) {
-			System.out.println("La Reserva ya fue aceptada previamente.");
-		}else {
-			reserva.aceptarReserva();
-			System.out.println("La Reserva fue aceptada correctamente.");
-		}
-	}
-	
-	//Rechazar la Reserva de un Inquilino
-	
-	public void cancelarReservaInquilino(Reserva reserva) {
-		if(!reserva.isConfirmada()) {
-			reserva.cancelarReserva();
-			System.out.println("La Reserva fue cancelada correctamente.");
-		}else {
-			System.out.println("La Reserva ya fue cancelada previamente.");
-		}
-	}
-	
-	//Verificar las Reservas Aceptadas.
-	
-	public List<Reserva> todasLasReservasAceptadas(){
-		List<Reserva> reservasAceptadas = new ArrayList<Reserva>();
-		for(Inmueble inmueble : this.getInmuebles()) {
-			reservasAceptadas.addAll(this.reservasAceptadasDelInmueble(inmueble));
-		}
-		return reservasAceptadas;
-	}
-	
-	public List<Reserva> reservasAceptadasDelInmueble(Inmueble inmueble){
-		return inmueble.getReservas().stream().filter(r -> r.isConfirmada()).toList();
-		
-	}
-	
-	//Agregar y eliminar un inmueble. 
-	
-	public void addInmueble(Inmueble inmueble) {
-		inmuebles.add(inmueble);
-	}
-	
-	public void eliminarInmueble(Inmueble inmueble) {
-		if(this.getInmuebles().contains(inmueble)) {
-			inmuebles.remove(inmueble);
-		}
-	}
-	
-	//Get
+import ar.edu.unq.po2.reserva.Reserva;
+import ar.edu.unq.po2.sistemaAlquiler.Inmueble;
 
-	public List<Inmueble> getInmuebles(){
-		return inmuebles;
-	}
+public interface  Propietario{
+	
+	public void aceptarReservaInquilino(Reserva reserva);
+	//public List<Inmueble> obtenerInmuebles();
+	//public void rankearInquilino(Inquilino inquilino, int puntaje);
+	
 
 }
--- src/ar/edu/unq/po2/usuario/Usuario.java
+++ src/ar/edu/unq/po2/usuario/Usuario.java
@@ -1,6 +1,8 @@
 package ar.edu.unq.po2.usuario;
 
-public class Usuario {
+import ar.edu.unq.po2.reserva.Reserva;
+
+public class Usuario implements Propietario{
 	
 	private String nombreCompleto;
 	private String telefono;
@@ -10,6 +12,20 @@ public class Usuario {
 		this.nombreCompleto = nombreCompleto;
 		this.telefono = telefono;
 		this.mail = mail;
+	}
+	
+	//Métodos para el Propietario
+	
+	//FALTA AGREGAR MÉTODOS.
+	
+	@Override
+	public void aceptarReservaInquilino(Reserva reserva) { //Ver esto como arreglarlo
+		if(reserva.isConfirmada()) {
+			System.out.println("La Reserva ya fue aceptada previamente.");
+		}else {
+			reserva.aceptarReserva();
+			System.out.println("La Reserva fue aceptada correctamente.");
+		}
 	}
 	
 }
