c859c053affca8fe1781ade552ee9d894d568fc6
danielcesaro <danielcesaro@homail.com>
2024-11-09 20:02:08 -0300

    CategoriaRankeo con un colaborador nuevo (testeada)

--- test/ar/edu/unq/po2/sistemaAlquiler/CategoriaRankeoTest.java
+++ test/ar/edu/unq/po2/sistemaAlquiler/CategoriaRankeoTest.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.po2.sistemaAlquiler;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,11 +9,12 @@ import org.junit.jupiter.api.Test;
 class CategoriaRankeoTest {
 	
 	private CategoriaRankeo categoria;
+	private IRankeable		entidad;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		
-		this.categoria = new CategoriaRankeo("Atención");
+		this.entidad   = mock(IRankeable.class);
+		this.categoria = new CategoriaRankeo("Atención", entidad);
 	}
 
 	@Test
@@ -23,6 +25,11 @@ class CategoriaRankeoTest {
 	@Test
 	void testGetNombre() {
 		assertEquals("Atención", categoria.getNombre());
+	}
+	
+	@Test
+	void testGetEntidad() {
+		assertEquals(entidad, categoria.getEntidad());
 	}
 
 }
