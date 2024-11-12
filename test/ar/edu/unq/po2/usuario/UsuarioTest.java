58d484e5c664dd2cd5284b5ea6ebdeab1cefaa10
axlg98 <axlgarcia080498@gmail.com>
2024-11-11 22:21:10 -0300

    Avanzando test Usuario

--- test/ar/edu/unq/po2/usuario/UsuarioTest.java
+++ test/ar/edu/unq/po2/usuario/UsuarioTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ar.edu.unq.po2.reserva.Reserva;
+import ar.edu.unq.po2.reserva.ReservaAceptada;
+import ar.edu.unq.po2.reserva.ReservaPendiente;
 import ar.edu.unq.po2.sistemaAlquiler.CategoriaRankeo;
 import ar.edu.unq.po2.sistemaAlquiler.Inmueble;
 import ar.edu.unq.po2.sistemaAlquiler.SAT;
@@ -20,23 +22,25 @@ import ar.edu.unq.po2.sistemaAlquiler.SAT;
 class UsuarioTest {
 	
 	//private Inquilino inquilino;
-	private Usuario inquilino;
-	private SAT sat;
-	private Ranking r1;
-	private Ranking r2;
-	private Ranking r3;
-	private Ranking r4;
-	private CategoriaRankeo	categoriaRankeo1;
-	private CategoriaRankeo	categoriaRankeo2;
-	private Inmueble inmueble;
-	private List<Inmueble> inmuebles;
-	private Reserva reserva;
+	 Usuario inquilino;
+	 Usuario propietario;
+	 SAT sat;
+	 Ranking r1;
+	 Ranking r2;
+	 Ranking r3;
+	 Ranking r4;
+	 CategoriaRankeo	categoriaRankeo1;
+	 CategoriaRankeo	categoriaRankeo2;
+	 Inmueble inmueble;
+	 List<Inmueble> inmuebles;
+	 Reserva reserva;
 	
 	
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		inquilino = new Usuario("Damian", "@@", "123");
+		propietario = new Usuario("Axel", "example@gmail.com", "91218");
 		sat = mock(SAT.class);
 		this.r1 = mock(Ranking.class);
 		this.r2 = mock(Ranking.class);
@@ -46,8 +50,61 @@ class UsuarioTest {
 		this.categoriaRankeo2	 = mock(CategoriaRankeo.class);
 		inmuebles = mock(List.class);
 		inmueble = mock(Inmueble.class);
+		reserva = mock(Reserva.class);
 	}
-
+	
+	@Test
+	void PropietarioRankeandoInquilinoTest() {
+		propietario.rankearInquilino(1, inmueble, "pésimo servicio");
+		
+		assertEquals(propietario.getRankings().size(),1);
+	}
+	
+	@Test
+	void InquilinoRankeandoPropietarioTest() {
+		inquilino.rankearPropietario(3, inmueble, "Regular");
+		
+		assertEquals(inquilino.getRankings().size(),1);
+	}
+	
+	@Test
+	void InquilinoRankeandoInmuebleTest() {
+		inquilino.rankearInmueble(5, inmueble, "Excelenteservicio");
+		
+		assertEquals(inquilino.getRankings().size(),1);
+	}
+	
+	@Test
+	void addRankingTest() {
+		propietario.addRanking(r1);
+		propietario.addRanking(r2);
+		propietario.addRanking(r3);
+		propietario.addRanking(r4);
+		
+		assertEquals(propietario.getRankings().size(),4);
+	}
+	
+	@Test
+	void aceptarReservaInquilinoTest() { // Arreglar
+		when(reserva.getEstado()).thenReturn(new ReservaPendiente());
+		propietario.aceptarReservaInquilino(reserva);
+		assertTrue(reserva.getEstado() instanceof ReservaPendiente);
+	}
+	
+	@Test
+	void rankearConPuntajeMayorA5Test() {
+		propietario.rankearInquilino(6, inmueble, "Excelente servicio");
+		assertEquals(propietario.getRankings().size(),0);
+	}
+	
+	@Test
+	void rankearConPuntajeMenorA1Test() {
+		propietario.rankearInquilino(0, inmueble, "Pésimo servicio");
+		assertEquals(propietario.getRankings().size(),0);
+	}
+	
+	//Inquilino
+	
 	@Test
 	void reservarInmuebleTest() {
 		// comprueba que se haya pasado el objeto de tipo Reserva y no el que se instancia
