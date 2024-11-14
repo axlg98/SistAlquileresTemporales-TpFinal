7a021ce74f6e56e009b5156f62dd541dcd9f2093
Damian Manchali <dami.manchali.eze@gmail.com>
2024-11-14 13:49:04 -0300

    eliminando clases sobrantes visualizacion y busqueda

--- src/ar/edu/unq/po2/busqueda/Busqueda.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.po2.busqueda;
-
-public class Busqueda {
-
-	public Busqueda() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- src/ar/edu/unq/po2/reserva/Visualizacion.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.po2.reserva;
-
-public class Visualizacion {
-
-	public Visualizacion() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
